c18f7f9f7f6ceadf35207f40889160ae5aba42c5
Richard James <richard@richardpjames.com>
2026-03-25 15:48:46 +0000

    Add syntax highlighting

--- functions.php
+++ functions.php
@@ -3,6 +3,18 @@
 function enqueue_styles()
 {
     wp_enqueue_style('tailwind', get_template_directory_uri() . '/css/tailwind.css', array(), filemtime(get_template_directory() . '/css/tailwind.css'));
+    wp_enqueue_script('highlightjs', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/highlight.min.js', array(), '11.11.1', true);
+    wp_add_inline_script('highlightjs', <<<'JS'
+document.addEventListener('DOMContentLoaded', () => {
+    if (typeof hljs === 'undefined') {
+        return;
+    }
+
+    document.querySelectorAll('pre code').forEach((block) => {
+        hljs.highlightElement(block);
+    });
+});
+JS);
 }
 
 add_action('wp_enqueue_scripts', 'enqueue_styles');
